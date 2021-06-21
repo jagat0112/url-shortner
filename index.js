59ebbf87f3a345ef0abe45463898653a47035c82
Jagat Pradhan <jagat.pradhan0112@gmail.com>
2021-06-21 08:53:49 +0545

    env PORT is assgined

--- index.js
+++ index.js
@@ -9,4 +9,5 @@ app.use(express.json());
 app.use("/api/url", require("./routes/url"));
 app.use("/", require("./routes/index"));
 
-app.listen(5000, () => console.log(`Server Started...`));
+
+app.listen(5000 || process.env.PORT, () => console.log(`Server Started...`));
