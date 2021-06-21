e5c3c75e12d20822ebf3db4f42f5fbf063ac7de6
Jagat Pradhan <jagat.pradhan0112@gmail.com>
2021-06-21 17:27:12 +0545

    cors added

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require("express");
-const helmet = require('helmet')
-const compression = require('compression')
+const helmet = require("helmet");
+const compression = require("compression");
+const cors = require("cors");
 
 const connectDB = require("./config/db");
 
 const app = express();
 connectDB();
 
-app.use(helmet())
-app.use(compression())
+app.use(helmet());
+app.use(compression());
+app.use(cors());
 
 app.use(express.json());
-
-
 
 app.use("/api/url", require("./routes/url"));
 app.use("/", require("./routes/index"));
 
-const PORT  = process.env.PORT
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server Started at ${PORT}...`));
